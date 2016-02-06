ba96c177526ba1fbe1dba54d061a981f4ff0a9af
BuddhistWalrus <Gabe>
2016-02-05 23:34:57 -0500

    styling for css/js

---
+++ app/assets/javascripts/main.js
@@ -0,0 +1,7 @@
+function colorBird() {
+  document.getElementById("bird").src = 'https://s3.amazonaws.com/eamonism/colorbird.png';
+}
+
+function grayscaleBird() {
+	document.getElementById("bird").src = 'https://s3.amazonaws.com/eamonism/bwbird.png';
+}
