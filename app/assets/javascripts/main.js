ef8dee8428fef6bc5c626dd414229bc07584ccf7
Gabriel Corso <gabe.17@dartmouth.edu>
2019-03-07 15:15:40 -0500

    new prices

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -14,28 +14,34 @@ var index;
 var images = [
 
 
-			  "https://s3.amazonaws.com/eamonism/Granadas.jpg",
+			  
 			  "https://s3.amazonaws.com/eamonism/Hunters.jpg",
 			  "https://s3.amazonaws.com/eamonism/Capragraph.jpg",
 
 			  "https://s3.amazonaws.com/eamonism/Augury.jpg",
 			  "https://s3.amazonaws.com/eamonism/Castilla.jpg", 
-			  "https://s3.amazonaws.com/eamonism/Illuminette.jpg",
 			  
 			  
-			  "https://s3.amazonaws.com/eamonism/GildedFish.jpg", 
-			  "https://s3.amazonaws.com/eamonism/Ornithograph.jpg",
 			  "https://s3.amazonaws.com/eamonism/MindsEyedrops.jpg", 
 
-			  "https://s3.amazonaws.com/eamonism/port10.jpg",
 			  "https://s3.amazonaws.com/eamonism/port11.jpg",
-			  "https://s3.amazonaws.com/eamonism/port12.jpg",
 
 			  
 			  "https://s3.amazonaws.com/eamonism/Rodentia.jpg", 
 			  "https://s3.amazonaws.com/eamonism/DreamofQueenMeabh.jpg", 
 			  "https://s3.amazonaws.com/eamonism/Psyght.jpg",
 
+
+			  "https://s3.amazonaws.com/eamonism/Ornithograph.jpg",
+
+
+			  "https://s3.amazonaws.com/eamonism/port12.jpg",
+			  "https://s3.amazonaws.com/eamonism/GildedFish.jpg", 
+
+			  "https://s3.amazonaws.com/eamonism/port10.jpg",
+
+			  "https://s3.amazonaws.com/eamonism/Granadas.jpg",
+			  "https://s3.amazonaws.com/eamonism/Illuminette.jpg",
 			  "https://s3.amazonaws.com/eamonism/Prospero.jpg",
 
 			  "https://s3.amazonaws.com/eamonism/birdSticker.png", 
@@ -46,23 +52,29 @@ var images = [
 	      		]
 
 // holds the titles of all the images
-var titles = [ 'Las Granadas - 11x17 inches', 'The Hunters - 11x15 inches', 'Capragraph - 11x14.5',
+var titles = [  'The Hunters (1st Ed.) - 11x15 inches', 'Capragraph (1st Ed.) - 11x14.5',
 
-				'Augury - 12x16 inches', 'Castilla - 11x14 inches', 'Illuminette - 10x15 inches',
+				'Augury (1st Ed.) - 12x16 inches', 'Castilla (1st Ed.) - 11x14 inches', 
 
-				'Ichthyograph - 17x11 inches', 'Ornithograph - 12x16 inches', "Mind's Eyedrops - 11x15 inches",
+				  "Mind's Eyedrops (1st Ed.) - 11x15 inches",
 
-				'Ornithograph ii - 12x16 inches',  'Savannah of the Waning Gibbous - 12x18 inches', 'The Font of Eschaton - 12x18 inches',
+				  'Savannah of the Waning Gibbous (1st Ed.) - 12x18 inches', 
 
-				'Rodentia - 18x12 inches', 'Dream of Queen Meabh - 12x18 inches', 'Psyght - 11x14 inches',
+				'Rodentia (1st Ed.) - 18x12 inches', 'Dream of Queen Meabh (1st Ed.) - 12x18 inches', 'Psyght (1st Ed.) - 11x14 inches',
 
-				'Prince Prospero - 10.25x14.5 inches',
+				 'Ornithograph (2nd Ed.) - 12x16 inches', 
+
+				 'The Font of Eschaton (2nd Ed.) - 12x18 inches', 'Ichthyograph (2nd Ed.) - 17x11 inches', 
+
+				 'Ornithograph ii (2nd Ed.) - 12x16 inches', 'Las Granadas (2nd Ed.) - 11x17 inches',
+
+				 'Illuminette (2nd Ed.) - 10x15 inches', 'Prince Prospero (2nd Ed.) - 10.25x14.5 inches', 
 
 				'Large Bird Sticker - 7x6.74 inches', 'Large Fish Sticker - 6.86x5.5 inches', 'Small Bird Sticker - 4x3.86 inches', 'Small Fish Sticker - 4x3.22 inches'];
 
 function startLightBox(event) {
 	index = parseInt(event.target.id);
-	if(index < 16){	// Get the modal
+	if(index < 9){	// Get the modal
 		var modal = document.getElementById('myModal');
 		
 
@@ -75,8 +87,8 @@ function startLightBox(event) {
 	    modalImg.src = images[index];
 
 	    var price = document.getElementById('price');
-	    price.innerHTML = "$12";
-	    document.getElementById('price_input').value = 12;
+	    price.innerHTML = "$20";
+	    document.getElementById('price_input').value = 20;
 
 	    var caption = document.getElementById("caption");
 	    caption.innerHTML = captionText;
@@ -89,6 +101,38 @@ function startLightBox(event) {
 		    modal.style.display = "none";
 		}
 	}
+	
+	else if(index < 16){	// Get the modal
+		var modal = document.getElementById('myModal');
+		
+
+		// Get the image and insert it inside the modal - use its "alt" text as a caption
+		var img = document.getElementById(index);
+		var modalImg = document.getElementById("img");
+		var captionText = titles[index];
+		document.getElementById('item_name').value = titles[index];
+	    modal.style.display = "block";
+	    modalImg.src = images[index];
+
+	    var price = document.getElementById('price');
+	    price.innerHTML = "$25";
+	    document.getElementById('price_input').value = 25;
+
+	    var caption = document.getElementById("caption");
+	    caption.innerHTML = captionText;
+
+		// Get the <span> element that closes the modal
+		var span = document.getElementsByClassName("close")[0];
+
+		// When the user clicks on <span> (x), close the modal
+		span.onclick = function() { 
+		    modal.style.display = "none";
+		}
+	}
+
+
+
+
 	else if(index < 18){	// Get the modal
 		var modal = document.getElementById('myModal');
 		
