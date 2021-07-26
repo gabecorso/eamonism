c33dc69738b2a3a6dea6dd224bddda323f817452
Gabe Corso <gabecorso@gmail.com>
2021-07-26 08:32:38 -0700

    new price

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -26,7 +26,7 @@ class WelcomeController < ApplicationController
 				shipping = 0.98
 			end
 			if params[:discount_code] == "MAILER1!"
-                price = (price*=(80/100)).round
+                price = (params[:price_input]*=(0.80)).round
             end
 		end
 
